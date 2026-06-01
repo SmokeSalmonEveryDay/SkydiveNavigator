7a48b95690d0f42bcb2f0bc51636c2eb8152e656
LiamOcean <liam.flavin@oceaninfinity.com>
2026-06-01 22:57:14 +0100

    Linting

--- SkydiveNavigator.app.js
+++ SkydiveNavigator.app.js
@@ -20,26 +20,22 @@ var flysightLogFile;            // File name for flysight log
 var flysightLogFrame;           // Log frame for flysight log
 var debugLogFile;               // File name for debug log
 var debugLogFrame;              // Log frame for debug log
-var lastAltitude = { alt: 0, time: new Date()};   // Last altitude (m) and time it was taken
+var lastAltitude = { alt: 0, time: new Date() };   // Last altitude (m) and time it was taken
 const sinkRatesList = [];
 const speedsList = [];
 
 // Add item to list, if there are more than 5 values then remove first value
-function addToList(list, item)
-{
-  if (list.length > 4)
-  {
+function addToList(list, item) {
+  if (list.length > 4) {
     list.shift();
   }
   list.push(item);
 }
 
 // Return average of list of numbers
-function listAverage(list)
-{
+function listAverage(list) {
   total = 0;
-  for(i = 0; i < list.length; i++)
-  {
+  for (i = 0; i < list.length; i++) {
     total += list[i];
   }
   average = total / list.length;
@@ -57,18 +53,18 @@ function radiansToDegrees(radians) {
 }
 
 function metresToFeet(metres) {
-  feet = metres *  3.2808;
+  feet = metres * 3.2808;
   return feet;
 }
 
-var netheravon = { lat: degreesToRadians(51.2428054), lon: degreesToRadians(-1.7620569), alt: 134.6};
-var langar = { lat: degreesToRadians(52.890602), lon: degreesToRadians(-0.905672), alt: 35.9};
-var sibson = { lat: degreesToRadians(52.561508), lon: degreesToRadians(-0.396838), alt: 40.0};
-var dunkeswell = { lat: degreesToRadians(50.863488), lon: degreesToRadians(-3.236506), alt: 250.0};
-var tilstock = { lat: degreesToRadians(52.932740), lon: degreesToRadians(-2.645203), alt: 92.0};
-var hibaldstow = { lat: degreesToRadians(53.497539), lon: degreesToRadians(-0.515410), alt: 9.0};
-var klatovy = { lat: degreesToRadians(49.418278), lon: degreesToRadians(13.321076), alt: 393.0};
-var dunkeswell = { lat: degreesToRadians(50.8630245), lon: degreesToRadians(-3.2363702), alt: 250.0};
+var netheravon = { lat: degreesToRadians(51.2428054), lon: degreesToRadians(-1.7620569), alt: 134.6 };
+var langar = { lat: degreesToRadians(52.890602), lon: degreesToRadians(-0.905672), alt: 35.9 };
+var sibson = { lat: degreesToRadians(52.561508), lon: degreesToRadians(-0.396838), alt: 40.0 };
+var dunkeswell = { lat: degreesToRadians(50.863488), lon: degreesToRadians(-3.236506), alt: 250.0 };
+var tilstock = { lat: degreesToRadians(52.932740), lon: degreesToRadians(-2.645203), alt: 92.0 };
+var hibaldstow = { lat: degreesToRadians(53.497539), lon: degreesToRadians(-0.515410), alt: 9.0 };
+var klatovy = { lat: degreesToRadians(49.418278), lon: degreesToRadians(13.321076), alt: 393.0 };
+var dunkeswell = { lat: degreesToRadians(50.8630245), lon: degreesToRadians(-3.2363702), alt: 250.0 };
 var dropzones = [netheravon, langar, sibson, dunkeswell, tilstock, hibaldstow, klatovy];
 
 // GPS lat and lon (rads)
@@ -78,8 +74,8 @@ var gpsRadians = {
 
 // Return distance between 2 points, not using great circle that's too hard for me (m)
 function getDistance(point1, point2) {
-  distance = Math.acos(Math.sin(point1.lat)*Math.sin(point2.lat)+Math.cos(point1.lat)*Math.cos(point2.lat)*Math.cos(point2.lon-point1.lon))*6371;
-  return distance*1000;
+  distance = Math.acos(Math.sin(point1.lat) * Math.sin(point2.lat) + Math.cos(point1.lat) * Math.cos(point2.lat) * Math.cos(point2.lon - point1.lon)) * 6371;
+  return distance * 1000;
 }
 
 // Returns bearing of point 2 from point 1 (degs, 0 - 360)
@@ -97,31 +93,27 @@ function getBearing(point1, point2) {
 function getReturnAltitude(gps, distance, dropzoneAlt, sinkRate, speed) {
   altitude = gps.alt - dropzoneAlt;
   timeToTarget = distance / (speed / 3.6);
-  returnAltitude = altitude - (timeToTarget * sinkRate); 
+  returnAltitude = altitude - (timeToTarget * sinkRate);
   return returnAltitude;
 }
 
 // Save sinkrate, +ve is down (m/s)
 // TODO: Calculate from baro, maybe fuse them
-function calculateSinkRate(gps)
-{
+function calculateSinkRate(gps) {
   deltaAltitude = lastAltitude.alt - gps.alt;
   deltaTime = gps.time.getTime() - lastAltitude.time.getTime();
-  if (deltaTime > 0)
-  {
+  if (deltaTime > 0) {
     lastAltitude.alt = gps.alt;
     lastAltitude.time = gps.time;
     calculatedSinkRate = deltaAltitude / (deltaTime / 1000);
-    if (calculatedSinkRate > 0)
-    {
+    if (calculatedSinkRate > 0) {
       addToList(sinkRatesList, calculatedSinkRate);
       return listAverage(sinkRatesList);
     }
   }
 }
 
-function calculateSpeed(speed)
-{
+function calculateSpeed(speed) {
   addToList(speedsList, speed);
   return listAverage(speedsList);
 }
@@ -132,10 +124,10 @@ function calculateSpeed(speed)
 // If heading towards dropzone, draw estimated height to reach dropzone at.
 function navigate(gps) {
   g.reset().clearRect(Bangle.appRect);
-  g.setFont("Vector", 60).setFontAlign(0,0,1);
+  g.setFont("Vector", 60).setFontAlign(0, 0, 1);
   g.clear();
 
-  if(gps.fix == 1) {
+  if (gps.fix == 1) {
     lastGPS = gps;
     speed = calculateSpeed(gps.speed);
     sinkRate = calculateSinkRate(gps);
@@ -150,7 +142,7 @@ function navigate(gps) {
     distance = getDistance(gpsRadians, dropzone);
     bearing = getBearing(gpsRadians, dropzone);
     deltaBearing = bearing - gps.course;
-    if (deltaBearing < -180){
+    if (deltaBearing < -180) {
       deltaBearing += 360;
     }
 
@@ -165,7 +157,7 @@ function navigate(gps) {
       }
     } else {
       returnAltitude = 0;
-      g.drawImage(require("Storage").read("arrow.img"),88,88,{rotate:degreesToRadians(deltaBearing)});
+      g.drawImage(require("Storage").read("arrow.img"), 88, 88, { rotate: degreesToRadians(deltaBearing) });
     }
 
     flysightLogFrame = {
@@ -181,19 +173,19 @@ function navigate(gps) {
       sAcc: 1,
       gpsFix: gps.fix,
       numSV: gps.satellites,
-      toString: function() {
+      toString: function () {
         return this.time + ","
-        + this.lat + "," 
-        + this.lon + ","
-        + this.alt + ","
-        + this.vn + ","
-        + this.ve + ","
-        + this.vd + ","
-        + this.hAcc + ","
-        + this.vAcc + ","
-        + this.sAcc + ","
-        + this.gpsFix + ","
-        + this.numSV + "\n";
+          + this.lat + ","
+          + this.lon + ","
+          + this.alt + ","
+          + this.vn + ","
+          + this.ve + ","
+          + this.vd + ","
+          + this.hAcc + ","
+          + this.vAcc + ","
+          + this.sAcc + ","
+          + this.gpsFix + ","
+          + this.numSV + "\n";
       }
     };
 
@@ -205,24 +197,24 @@ function navigate(gps) {
       speed: gps.speed * 0.36,
       course: gps.course,
       hdop: gps.hdop,
-      numSV: gps.satellites, 
+      numSV: gps.satellites,
       sinkRate: sinkRate,
       distance: distance,
       deltaBearing: deltaBearing,
       returnAltitude: returnAltitude,
-      toString: function() {
+      toString: function () {
         return this.time + ","
-        + this.lat + "," 
-        + this.lon + ","
-        + this.alt + ","
-        + this.speed + ","
-        + this.course + ","
-        + this.hdop + ","
-        + this.numSV + ","
-        + this.sinkRate + ","
-        + this.distance + ","
-        + this.deltaBearing + ","
-        + this.returnAltitude + "\n";
+          + this.lat + ","
+          + this.lon + ","
+          + this.alt + ","
+          + this.speed + ","
+          + this.course + ","
+          + this.hdop + ","
+          + this.numSV + ","
+          + this.sinkRate + ","
+          + this.distance + ","
+          + this.deltaBearing + ","
+          + this.returnAltitude + "\n";
       }
     };
 
@@ -238,8 +230,7 @@ function navigate(gps) {
 }
 
 // Return closest dropzone
-function getDropzone(gpsRadians)
-{
+function getDropzone(gpsRadians) {
   smallestDistanceIndex = 0;
   smallestDistance = 1000000;
   for (i = 0; i < dropzones.length; i++) {
@@ -280,7 +271,7 @@ var loggingStartTime;
 function initialiseFlysightLog() {
   date = new Date();
   logTitle = "FS_" + date.toISOString();
-  flysightLogFile = require("Storage").open(logTitle,"a");
+  flysightLogFile = require("Storage").open(logTitle, "a");
   flysightLogFile.write("time,lat,lon,hMSL,velN,velE,velD,hAcc,vAcc,sAcc,gpsFix,numSV,(deg),(deg),(m),(m/s),(m/s),(m/s),(m),(m),(m/s),,,");
 }
 
@@ -288,44 +279,40 @@ function initialiseFlysightLog() {
 function initialiseDebugLog() {
   date = new Date();
   logTitle = "Dbug_" + date.toISOString();
-  debugLogFile = require("Storage").open(logTitle,"a");
+  debugLogFile = require("Storage").open(logTitle, "a");
   loggingStartTime = Date.now();
   debugLogFile.write("time, lat, lon, altAGL (m), speed (m/s), course (degs), hdop, satellites, sink rate raw (m/s), DZ distance (m), DZ delta bearing (degs), DZ return (m) alt\n");
 }
 
-function displaySplash()
-{
+function displaySplash() {
   g.reset().clearRect(Bangle.appRect);
-  g.setFont("Vector", 32).setFontAlign(0,0,1);
+  g.setFont("Vector", 32).setFontAlign(0, 0, 1);
   g.drawString("Skydive\nNav\nv1.1.0", 88, 88);
 }
 
 //Write logFrame as csv line on logFile
-function log(logFile, logFrame)
-{
+function log(logFile, logFrame) {
   logFile.write(logFrame.toString());
 }
 
-function startup()
-{
+function startup() {
   displaySplash();
   // initialiseBarometerID = setInterval(initialiseBarometer, 500);
 }
 
-function main()
-{
+function main() {
   g.reset().clearRect(Bangle.appRect);
-  g.setFont("Vector", 32).setFontAlign(0,0,1);
+  g.setFont("Vector", 32).setFontAlign(0, 0, 1);
   g.drawString("Waiting\nFor\nButton...", 88, 88);
-  setWatch(function() {
+  setWatch(function () {
     // checkActivateOnAltitude = false;
     // Bangle.setBarometerPower(0, "app");
     Bangle.setGPSPower(1, "app");
-    Bangle.on('GPS', function(gps) { navigate(gps); });
+    Bangle.on('GPS', function (gps) { navigate(gps); });
     initialiseFlysightLog();
     initialiseDebugLog();
     Bangle.buzz(200);
-  }, BTN, {edge: "rising", debounce:100, repeat:false});
+  }, BTN, { edge: "rising", debounce: 100, repeat: false });
 }
 
 // Allow baro 1 second after power up before taking ground pressure reading
